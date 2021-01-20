45594ec665407c1b5e33bffb2cbb4fcd2816acb4
Alex_XU <jiakai.xu@my.cityu.edu.hk>
2021-01-20 10:38:54 +0800

    Testing from Alex

--- Project1/test1.cpp
+++ Project1/test1.cpp
@@ -3,5 +3,11 @@ using namespace std;
 
 int main() {
 	cout << "Hello World!" << endl;
+	int command = 0;
+	cout << "Your command here:"<< endl;
+	cin >> command;
+	if(command == 0){
+		cout << "No command received!"<< endl;
+	}
 	return 0;
 }
